5a868860917520cc532dce140bcef1f6a10d32b2
Prasanna Venkatesh Kumar RTV <70711932+pvk13797@users.noreply.github.com>
2021-02-10 11:03:38 +0530

    Add files via upload

---
+++ js/PopUp.js
@@ -0,0 +1,7 @@
+function clickBox() {
+    document.getElementById("appearBox").style.display = "block";
+}
+
+function clickClose() {
+    document.getElementById("appearBox").style.display = "none";
+}
